274ba4ba31e129de51f911ec04f0e2b651d4ba1d
keiner lasprilla <klasprilla12@gmail.com>
2026-06-02 16:57:00 -0500

    ueb

---
+++ service-worker.js
@@ -0,0 +1,32 @@
+const CACHE_NAME = 'bodega-cache-v1';
+const ASSETS = [
+  '/',
+  '/index.html',
+  '/manifest.json',
+  '/assets/images.jpg',
+  '/public/css/styles.css'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+  );
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(
+        keys.filter((key) => key !== CACHE_NAME).map((key) => caches.delete(key))
+      )
+    )
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((cachedResponse) => {
+      return cachedResponse || fetch(event.request);
+    })
+  );
+});
